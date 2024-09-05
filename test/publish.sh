890c290c3508c8f0ec1c18ebcd668a1b403bc2ae
Aaron Wislang <aaron.wislang@microsoft.com>
2024-09-05 15:54:31 -0400

    Update test/publish.sh
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- test/publish.sh
+++ test/publish.sh
@@ -3,17 +3,20 @@
 # login to the registry
 # if running locally, we may need to: 
 # gh auth refresh -h github.com -s write:packages,read:packages
-gh auth token | docker login ghcr.io --username YOURUSERNAME --password-stdin
 
-# publish using wkg
-PROJECT_NAME="rust_wasi_hello"
+# gh auth token | docker login ghcr.io --username YOURUSERNAME --password-stdin
 
-if [ -z "$IMAGE_NAME" ]; then
-  GH_USER=$(gh api user --jq '.login')
-  IMAGE_NAME="${GH_USER}/rust-wasi-hello"
-  export IMAGE_NAME
-fi
+# # publish using wkg
+# PROJECT_NAME="rust_wasi_hello"
+
+# if [ -z "$IMAGE_NAME" ]; then
+#   GH_USER=$(gh api user --jq '.login')
+#   IMAGE_NAME="${GH_USER}/rust-wasi-hello"
+#   export IMAGE_NAME
+# fi
 
 REGISTRY_REFERENCE="ghcr.io/${IMAGE_NAME}:latest"
 
-wkg oci push $REGISTRY_REFERENCE target/wasm32-wasip1/release/$PROJECT_NAME.wasm
+echo "REGISTRY_REFERENCE: ${REGISTRY_REFERENCE}"
+
+wkg oci push $REGISTRY_REFERENCE test/$PROJECT_NAME.wasm
